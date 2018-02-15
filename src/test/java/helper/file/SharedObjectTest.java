d651450932d29cc62477d6acac8d4999145b484b
ukt <556655qwer>
2018-02-15 23:00:02 +0200

    check generic class cast exception;

--- src/test/java/helper/file/SharedObjectTest.java
+++ src/test/java/helper/file/SharedObjectTest.java
@@ -25,7 +25,7 @@ public class SharedObjectTest {
             .isNull();
     }
 
-    @Test
+    @Test(expectedExceptions = ClassCastException.class)
     public void testLoadIncorrectValue() {
         SharedObject.save(testFileUrl, "Test");
         Assertions
