f3847dac3f07319bad3d0dc724dbccc2dedda5d7
ukt <556655qwer>
2018-05-16 01:56:13 +0300

    add upsource user map ids on name

--- src/main/java/telegram/bot/data/UpsourceData.java
+++ src/main/java/telegram/bot/data/UpsourceData.java
@@ -1,15 +1,25 @@
 package telegram.bot.data;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class UpsourceData {
     public final String url;
     public final String login;
     public final String pass;
+    public final Map<String, String> userIdOnNameMap;
 
     public UpsourceData(Properties properties) {
         url = properties.getProperty("upsource.url");
         login = properties.getProperty("upsource.auth.login");
         pass = properties.getProperty("upsource.auth.pass");
+        userIdOnNameMap = new HashMap<>();
+        String userIdsMap = properties.getProperty("upsource.userIdsMap");
+        for (String userIdsOnName : userIdsMap.split(",")) {
+            String[] values = userIdsOnName.split(":");
+            userIdOnNameMap.put(values[0], values[1]);
+        }
+
     }
 }
