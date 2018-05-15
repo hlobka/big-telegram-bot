5fa622477a9aeb56d82398aff31dfd03eaafcb6e
ukt <556655qwer>
2018-05-15 22:04:04 +0300

    add to TimeHelper.java getMinutesUntilTarget

--- src/main/java/helper/time/TimeHelper.java
+++ src/main/java/helper/time/TimeHelper.java
@@ -1,6 +1,7 @@
 package helper.time;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class TimeHelper {
 
@@ -11,5 +12,15 @@ public class TimeHelper {
             hour -= 24;
         }
         return targetHour - hour;
+    }
+
+    public static Long getMinutesUntilTargetHour(int targetHour) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+        while (hour >= targetHour){
+            hour -= 24;
+        }
+        return TimeUnit.HOURS.toMinutes(targetHour - hour) + minutes;
     }
 }
--- src/test/java/helper/time/TimeHelperTest.java
+++ src/test/java/helper/time/TimeHelperTest.java
@@ -3,6 +3,8 @@ package helper.time;
 import org.testng.annotations.Test;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import static org.testng.Assert.*;
 
@@ -35,6 +37,45 @@ public class TimeHelperTest {
                 targetHour -= 24;
             }
             assertEquals(i, TimeHelper.getHoursUntilTarget(targetHour));
+        }
+    }
+
+    @Test
+    public void testGetMinutesUntilTargetHour() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+        TimeUnit hours = TimeUnit.HOURS;
+        Function<Long, Long> hoursToMinutes = aHour -> minutes + hours.toMinutes(aHour);
+        assertEquals(hoursToMinutes.apply(24L), TimeHelper.getMinutesUntilTargetHour(hour));
+        assertEquals(hoursToMinutes.apply(1L), TimeHelper.getMinutesUntilTargetHour(hour + 1));
+        assertEquals(hoursToMinutes.apply(23L), TimeHelper.getMinutesUntilTargetHour(hour - 1));
+    }
+
+    @Test
+    public void testGetMinutesUntilTargetHourByUsingWrongTime() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+        TimeUnit hours = TimeUnit.HOURS;
+        Function<Long, Long> hoursToMinutes = aHour -> minutes + hours.toMinutes(aHour);
+        assertEquals(hoursToMinutes.apply(48L), TimeHelper.getMinutesUntilTargetHour(hour + 24 * 2));
+        assertEquals(TimeHelper.getHoursUntilTarget(hour), TimeHelper.getHoursUntilTarget(hour - 24 * 2));
+    }
+
+    @Test
+    public void testGetMinutesUntilTargetHourWorksWellForAllAvailableTimes() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        for (long i = 1; i <= 24; i++) {
+            long targetHour = hour + i;
+            if (targetHour > 24) {
+                targetHour -= 24;
+            }
+            int minutes = calendar.get(Calendar.MINUTE);
+            TimeUnit hours = TimeUnit.HOURS;
+            Function<Long, Long> hoursToMinutes = aHour -> minutes + hours.toMinutes(aHour);
+            assertEquals(hoursToMinutes.apply(i), TimeHelper.getMinutesUntilTargetHour((int) targetHour));
         }
     }
 }
