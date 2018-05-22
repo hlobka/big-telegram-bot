646a3b5d40e675ba7eb5afea8d7b606506cdc92b
ukt <556655qwer>
2018-05-22 20:31:06 +0300

    extract ShowInformationFromResource.java from duplicates code

--- src/main/java/telegram/bot/commands/ShowHelp.java
+++ src/main/java/telegram/bot/commands/ShowHelp.java
@@ -1,25 +1,12 @@
 package telegram.bot.commands;
 
-import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ParseMode;
-import helper.string.StringHelper;
-import javafx.util.Pair;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 
 import static telegram.bot.data.Common.HELP_LINK;
 
-public class ShowHelp implements Command {
+public class ShowHelp extends ShowInformationFromResource {
 
-    @Override
-    public Pair<ParseMode, List<String>> run(Update update, String values) {
-        try {
-            return new Pair<>(ParseMode.HTML, Collections.singletonList(StringHelper.getFileAsString(HELP_LINK)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new Pair<>(ParseMode.HTML, Collections.singletonList("Big bot is a telegram bot to help organize work in team"));
+    public ShowHelp() {
+        super(HELP_LINK, ParseMode.HTML);
     }
 }
--- src/main/java/telegram/bot/commands/ShowHelpLinks.java
+++ src/main/java/telegram/bot/commands/ShowHelpLinks.java
@@ -2,29 +2,16 @@ package telegram.bot.commands;
 
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ParseMode;
-import helper.string.StringHelper;
-import javafx.util.Pair;
 import telegram.bot.data.Common;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 
 import static telegram.bot.data.Common.BIG_GENERAL_GROUP_IDS;
 
-public class ShowHelpLinks implements Command {
-    @Override
-    public Pair<ParseMode, List<String>> run(Update update, String values) {
-        try {
-            String helpLink = getHelpLink(update);
-            return new Pair<>(ParseMode.HTML, Collections.singletonList(StringHelper.getFileAsString(helpLink)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new Pair<>(ParseMode.HTML, Collections.singletonList("Big bot is a telegram bot to help organize work in team"));
+public class ShowHelpLinks extends ShowInformationFromResource {
+    public ShowHelpLinks() {
+        super(ShowHelpLinks::getHelpLink, ParseMode.HTML);
     }
 
-    private String getHelpLink(Update update) {
+    private static String getHelpLink(Update update) {
         return BIG_GENERAL_GROUP_IDS.contains(update.message().chat().id()) ? Common.BIG_HELP_LINKS : Common.HELP_LINKS;
     }
 }
