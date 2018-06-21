da1be93496209eb0dddd2019c62d2b4081769f6d
ukt <556655qwer>
2018-06-21 19:28:21 +0300

    regexp stabilization for math expression

--- src/main/java/telegram/bot/rules/AnswerRule.java
+++ src/main/java/telegram/bot/rules/AnswerRule.java
@@ -180,7 +180,7 @@ public class AnswerRule implements Rule {
         commonRegAnswers.put("^[a-zа-я]{21,}$", s -> "Ну очень длинное слово");
         commonRegAnswers.put("(.*(Ф|ф)а+к,? .*)|(^(Ф|ф)а+к!{0,})", s -> "Попрошу не выражаться.");
         commonRegAnswers.put("^(M|m)erde$", s -> "Pue");
-        commonRegAnswers.put("[(0-9+-/*^)(]+", s -> {
+        commonRegAnswers.put("^[(0-9+-/*^]{3,}$|^[(0-9+-/*^)(]{5,}$", s -> {
             try {
                 return StringMath.stringToMathResult(s) + "";
             } catch (NumberFormatException e) {
