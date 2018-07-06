c5eb89a3ee56d73d51a2c4bb1b37343e95e934db
ukt <556655qwer>
2018-07-06 14:54:09 +0300

    [Jenkins] try to fix ets checker

--- src/main/java/telegram/bot/checker/EtsClarityChecker.java
+++ src/main/java/telegram/bot/checker/EtsClarityChecker.java
@@ -1,13 +1,16 @@
 package telegram.bot.checker;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.ChatMember;
 import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.EditMessageText;
+import com.pengrad.telegrambot.request.GetChatMember;
 import com.pengrad.telegrambot.request.GetChatMembersCount;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetChatMemberResponse;
 import com.pengrad.telegrambot.response.GetChatMembersCountResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 import helper.file.SharedObject;
@@ -66,6 +69,7 @@ public class EtsClarityChecker extends Thread {
     private void check() throws IOException {
         System.out.println("EtsClarityChecker::check");
         if (checkIsFriday()) {
+            checkIsAllUsersPresentsOnThisChat(bot);
             if (checkIsResolvedToDay(bot)) {
                 if(!isResolvedToday){
                     isResolvedToday = true;
@@ -190,6 +194,61 @@ public class EtsClarityChecker extends Thread {
         int expectedCount = count - 1 - usersInVacation.size();
         isResolvedToday = resolvedCount >= expectedCount;
         return resolvedUsers.toString() + String.format("%nResolved: %d/%d%n", resolvedCount, count - 1);
+    }
+
+    public static void checkIsAllUsersPresentsOnThisChat(TelegramBot bot) {
+        HashMap<User, Boolean> users = SharedObject.loadMap(ETS_USERS, new HashMap<User, Boolean>());
+        ArrayList<User> usersInVacation = SharedObject.loadList(ETS_USERS_IN_VACATION, new ArrayList<User>());
+        List<Integer> allUsersId = new ArrayList<>();
+
+        List<User> usersToRemove = new ArrayList<>();
+        List<User> usersToRemoveFromVacation = new ArrayList<>();
+        for (User user : users.keySet()) {
+            if(!allUsersId.contains(user.id())) {
+                allUsersId.add(user.id());
+            }
+        }
+        for (User user : usersInVacation) {
+            if(!allUsersId.contains(user.id())) {
+                allUsersId.add(user.id());
+            }
+        }
+        for (Integer userId : allUsersId) {
+            GetChatMemberResponse response = bot.execute(new GetChatMember(Common.BIG_GENERAL_CHAT_ID, userId));
+            ChatMember chatMember = response.chatMember();
+            if(chatMember ==null || chatMember.status().equals(ChatMember.Status.left)) {
+                for (User user : users.keySet()) {
+                    if(user.id().equals(userId)) {
+                        usersToRemove.add(user);
+                    }
+                }
+                for (User user : usersInVacation) {
+                    if(user.id().equals(userId)) {
+                        usersToRemoveFromVacation.add(user);
+                    }
+                }
+            }
+        }
+        for (User userFromVacation : usersInVacation) {
+            for (User user : users.keySet()) {
+                if(user.id().equals(userFromVacation.id()) && !user.equals(userFromVacation)){
+                    usersToRemoveFromVacation.add(userFromVacation);
+                }
+            }
+        }
+        for (User user : usersToRemove) {
+            users.remove(user);
+        }
+        for (User user : usersToRemoveFromVacation) {
+            usersInVacation.remove(user);
+        }
+
+        if(!usersToRemoveFromVacation.isEmpty()) {
+            SharedObject.save(ETS_USERS_IN_VACATION, usersInVacation);
+        }
+        if(!usersToRemove.isEmpty()) {
+            SharedObject.save(ETS_USERS, users);
+        }
     }
 
     public static Boolean checkIsResolvedToDay(TelegramBot bot) {
