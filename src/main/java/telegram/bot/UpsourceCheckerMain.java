729eafb756b059b9b62606ab4dc3bb6d325c18c8
ukt <556655qwer>
2018-07-18 00:08:52 +0300

    [Jira] combine jira reviewer with upsource review;

---
+++ src/main/java/telegram/bot/UpsourceCheckerMain.java
@@ -0,0 +1,18 @@
+package telegram.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import telegram.bot.checker.UpsourceChecker;
+import telegram.bot.data.Common;
+import telegram.bot.data.chat.ChatData;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class UpsourceCheckerMain {
+
+    public static void main(String[] args) throws IOException {
+        TelegramBot bot = new TelegramBot(Common.data.token);
+        ChatData chatData = new ChatData(Common.TEST_FOR_BOT_GROUP_ID, null, Collections.singletonList("wildfury"));
+        new UpsourceChecker(bot).check(chatData);
+    }
+}
