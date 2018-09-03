523622e13940a04062130759ba09122dcee9bf9c
ukt <556655qwer>
2018-09-03 13:07:01 +0300

    [SharedObject] provide new api to work with instance dependent value;

--- src/main/java/helper/file/SharedObject.java
+++ src/main/java/helper/file/SharedObject.java
@@ -1,5 +1,7 @@
 package helper.file;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -56,7 +58,13 @@ public class SharedObject {
 
     private static void removeFile(String url) {
         if (new File(url).exists()) {
-            new File(url).delete();
+            if(!new File(url).delete()){
+                try {
+                    FileUtils.deleteDirectory(new File(url));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -65,16 +73,20 @@ public class SharedObject {
     }
 
     public static <T> T load(String url, Class<T> eClass) {
+        return loadOrDefault(url, eClass, null);
+    }
+
+    public static <T> T loadOrDefault(String url, Class<T> eClass, T defaultValue) {
         T result;
         if (!checkIsExist(url)) {
-            return null;
+            return defaultValue;
         }
         createNewFile(url);
         try (FileInputStream fileIn = new FileInputStream(url); ObjectInputStream in = new ObjectInputStream(fileIn)) {
             result = (T) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-            return null;
+            return defaultValue;
         }
         return result;
     }
@@ -86,5 +98,36 @@ public class SharedObject {
     public static <E> ArrayList<E> loadList(String url, ArrayList<E> defaultValue) {
         ArrayList result = load(url, ArrayList.class);
         return result == null ? defaultValue : result;
+    }
+
+    public static void save(Object instance, String sharedObjectName, Serializable data) {
+        save(instance.getClass(), sharedObjectName, data);
+    }
+
+    public static void save(Class<?> instanceClass, String sharedObjectName, Serializable data) {
+        String url = getClassUrl(instanceClass, sharedObjectName);
+        save(url, data);
+    }
+
+    public static <T> T load(Object instance, String sharedObjectName, Class<T> eClass, T defaultValue) {
+        return load(instance.getClass(), sharedObjectName, eClass, defaultValue);
+    }
+
+    public static <T> T load(Object instance, String sharedObjectName, Class<T> eClass) {
+        return load(instance.getClass(), sharedObjectName, eClass);
+    }
+
+    public static <T> T load(Class<?> instanceClass, String sharedObjectName, Class<T> eClass) {
+        return load(instanceClass, sharedObjectName, eClass, null);
+    }
+
+    public static <T> T load(Class<?> instanceClass, String sharedObjectName, Class<T> eClass, T defaultValue) {
+        String url = getClassUrl(instanceClass, sharedObjectName);
+        return loadOrDefault(url, eClass, defaultValue);
+    }
+
+    public static String getClassUrl(Class<?> instanceClass, String sharedObjectName) {
+        String simpleName = instanceClass.getSimpleName();
+        return "/tmp/" + simpleName + "/" + sharedObjectName + ".ser";
     }
 }
