6a1d2def4823515564fe4c1b2c283a1b546f99de
ukt <556655qwer>
2018-09-27 21:21:16 +0300

    [Jira] attempt to increase socket timeout

--- src/main/java/atlassian/jira/JiraHelper.java
+++ src/main/java/atlassian/jira/JiraHelper.java
@@ -1,17 +1,23 @@
 package atlassian.jira;
 
+import com.atlassian.httpclient.apache.httpcomponents.ApacheAsyncHttpClient;
+import com.atlassian.httpclient.api.factory.HttpClientOptions;
+import com.atlassian.jira.rest.client.api.AuthenticationHandler;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.JiraRestClientFactory;
 import com.atlassian.jira.rest.client.api.RestClientException;
 import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.atlassian.jira.rest.client.auth.BasicHttpAuthenticationHandler;
+import com.atlassian.jira.rest.client.internal.async.*;
 import com.atlassian.util.concurrent.Promise;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import telegram.bot.data.LoginData;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class JiraHelper {
@@ -59,14 +65,26 @@ public class JiraHelper {
 
     public static JiraHelper getClient(LoginData loginData, Boolean useCache) {
         try {
-            JiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
             URI uri = new URI(loginData.url);
-            JiraRestClient client = factory.createWithBasicHttpAuthentication(uri, loginData.login, loginData.pass);
+            JiraRestClient client = getJiraRestClient(uri, loginData.login, loginData.pass);
             return getClient(client, useCache);
         } catch (URISyntaxException ex) {
             ex.printStackTrace();
             throw new RuntimeException(ex);
         }
+    }
+
+    public static AsynchronousJiraRestClient getJiraRestClient(URI serverUri, String username, String password) {
+        AuthenticationHandler authenticationHandler = (new BasicHttpAuthenticationHandler(username, password));
+        DisposableHttpClient httpClient = (new AsynchronousHttpClientFactory()).createClient(serverUri, authenticationHandler);
+        AtlassianHttpClientDecorator atlassianHttpClientDecorator = (AtlassianHttpClientDecorator) httpClient;
+        try {
+            ApacheAsyncHttpClient httpClient1 = (ApacheAsyncHttpClient) FieldUtils.readField(atlassianHttpClientDecorator, "httpClient", true);
+            ((HttpClientOptions) FieldUtils.readField(httpClient1, "httpClientOptions", true)).setSocketTimeout(2, TimeUnit.MINUTES);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return new AsynchronousJiraRestClient(serverUri, httpClient);
     }
 
     public static JiraHelper getClient(JiraRestClient client, Boolean useCache) {
