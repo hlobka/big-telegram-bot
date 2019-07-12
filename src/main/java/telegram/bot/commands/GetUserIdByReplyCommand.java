01c49a8021aa17c4c2b56e0f00a56200682e879d
hsavenko <Hlib.Savenko@IGT.com>
2019-07-13 02:13:56 +0300

    [Telegram] | fix of GetUserIdByReplyCommand.

--- src/main/java/telegram/bot/commands/GetUserIdByReplyCommand.java
+++ src/main/java/telegram/bot/commands/GetUserIdByReplyCommand.java
@@ -5,12 +5,9 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import javafx.util.Pair;
-import telegram.bot.data.Common;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class GetUserIdByReplyCommand implements Command {
 
@@ -18,7 +15,7 @@ public class GetUserIdByReplyCommand implements Command {
     public Pair<ParseMode, List<String>> run(Update update, String values) {
         Message replyToMessage = update.message().replyToMessage();
         if (replyToMessage != null) {
-            User replyUser = replyToMessage.from();
+            User replyUser = replyToMessage.forwardFrom() != null ? replyToMessage.forwardFrom() : replyToMessage.from();
             return new Pair<>(ParseMode.Markdown, Collections.singletonList(String.format("user id is : %d", replyUser.id())));
         }
 
